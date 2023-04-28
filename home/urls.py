1301137b7da65d8e57f5153fe502e503647aff0f
Ragnar2001 <adityaholkar55555@gmail.com>
2023-04-28 13:24:40 +0530

    yes

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("home/", views.home, name="home"),
+    # path("", views.index, name="index"),
+    path("", views.home, name="home"),
     path("about/", views.about, name="about"),
 ]
